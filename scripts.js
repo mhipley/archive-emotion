880e2ec83e89ade8b1c32992c7ee717b237deaf1
Martha Hipley <info@marthahipley.com>
2023-03-21 17:16:45 -0600

    new UI demo

--- scripts.js
+++ scripts.js
@@ -1,53 +1,41 @@
+var a = document.getElementById("audio");
+var v = document.getElementById("video");
+var vPrev = document.getElementById("video-prev");
+var vNext = document.getElementById("video-next");
+var canvas = document.getElementById("c");
+var context = canvas.getContext("2d");
+var back = document.createElement("canvas");
+var backcontext = back.getContext("2d");
+var cw, ch, scale;
+cw = window.innerWidth;
+ch = window.innerHeight;
+if (cw >= ch) {
+  canvas.width = ch;
+  canvas.height = ch;
+} else {
+  canvas.width = cw;
+  canvas.height = cw;
+}
 
-    
-    
-    var v = document.getElementById('video');
-    var a = document.getElementById('audio');
-    var canvas = document.getElementById('c');
-    var context = canvas.getContext('2d');
-    var back = document.createElement('canvas');
-    var backcontext = back.getContext('2d');
-    var cw,ch,scale;
-    cw = window.innerWidth;
-    ch = window.innerHeight;
-    console.log(cw);
-    console.log(ch);
-    if (cw >= ch) {
-        canvas.width = ch;
-        canvas.height = ch;
-    }
-    else {
-        canvas.width = cw;
-        canvas.height = cw;
-    }
+window.addEventListener("resize", () => {
+  console.log("resized");
+  cw = window.innerWidth;
+  ch = window.innerHeight;
+});
 
-
-    window.addEventListener('resize', () => {
-        console.log('resized');
-        cw = window.innerWidth;
-        ch = window.innerHeight;
-
-    });
- 
-    let MAX_ZOOM = 1
-    let MIN_ZOOM = 0.1
-    let cameraOffset = { x: cw/2, y: ch/2 }
-    var preload = new Image();
-    preload.onload = function() {
-        context.scale(MIN_ZOOM, MIN_ZOOM);
-        if (cw >= ch) {
-        context.drawImage(preload, 0, 0, ch, ch);
-        context.translate( ch / 2, ch / 2 );
-        context.translate( -ch / 2 + cameraOffset.y, -ch / 2 + cameraOffset.y );
-        }
-        else {
-            context.drawImage(preload, 0, 0, cw, cw);
-            context.translate( cw / 2, cw / 2 );
-            context.translate( -cw / 2 + cameraOffset.x, -cw / 2 + cameraOffset.x );
-        }
-    };
-    preload.src = "/images/still.png";
-   
+let MAX_ZOOM = 1;
+let MIN_ZOOM = 1;
+let cameraOffset = { x: cw / 2, y: ch / 2 };
+var preload = new Image();
+preload.onload = function () {
+  context.scale(MIN_ZOOM, MIN_ZOOM);
+  if (cw >= ch) {
+    context.drawImage(preload, 0, 0, ch, ch);
+  } else {
+    context.drawImage(preload, 0, 0, cw, cw);
+  }
+};
+preload.src = "/images/still.png";
     
     
 
@@ -98,124 +86,186 @@
         ]
     }
 
-    const initIndex = 0;
-    var curIndex = initIndex;
-    var song = content.songs[initIndex];
-    var aFile = song.audio;
-    var vFile = song.video;
-    v.src = vFile;
-    a.src = aFile;
+    // const initIndex = 0;
+    // var curIndex = initIndex;
+    // if (curIndex > 0) {
+    //   var prevIndex = curIndex - 1;
+    // } else {
+    //   var prevIndex = 6;
+    // }
+    // if (curIndex < 6) {
+    //   var nextIndex = curIndex + 1;
+    // } else {
+    //   var nextIndex = 1;
+    // }
+    // var song = content.songs[initIndex];
+    // var songPrev = content.songs[prevIndex];
+    // var songNext = content.songs[nextIndex];
+    // var aFile = song.audio;
+    // var vFile = song.video;
+    // var vFileNext = songNext.video;
+    // var vFilePrev = songPrev.video;
+    // v.src = vFile;
+    // vPrev.src = vFilePrev;
+    // vNext.src = vFileNext;
+    // a.src = aFile;
+    
 
-    v.addEventListener('play', () => {
-        
+    v.addEventListener(
+      "play",
+      () => {
         if (cw >= ch) {
-            console.log("landscape");
-            scale = ch;
-            draw(v,context,backcontext,scale);
-            playMusic();
+          console.log("landscape");
+          scale = ch;
+          draw(v, context, backcontext, scale);
+          playMusic();
+        } else {
+          console.log("portrait");
+          scale = cw;
+          draw(v, context, backcontext, scale);
+          playMusic();
         }
-        else {
-            console.log("portrait");
-            scale = cw;
-            draw(v,context,backcontext,scale);
-            playMusic();
-        }
-
-
-
-        
-    },false);
-
-
-
-    v.addEventListener('pause', () => {
+      },
+      false
+    );
+    
+    v.addEventListener(
+      "pause",
+      () => {
         pauseMusic();
-    },false);
-
-
-
+      },
+      false
+    );
     
     function playMusic() {
-        a.play();
+      a.play();
     }
-
+    
     function pauseMusic() {
-        a.pause();
+      a.pause();
     }
-
     
-    function draw(v,c,bc,scale) {
-        canvas.width = scale;
-        canvas.height = scale;
-        back.width = scale;
-        back.height = scale;
-        if(v.paused || v.ended) return false;
-        // First, draw it into the backing canvas
-        bc.drawImage(v,0,0,scale,scale);
-        // Grab the pixel data from the backing canvas
-        var idata = bc.getImageData(0,0,scale,scale);
-        var data = idata.data;
-        // Loop through the pixels, turning them grayscale
-        for(var i = 0; i < data.length; i+=4) {
-            // var r = data[i];
-            // var g = data[i+1];
-            // var b = data[i+2];
-            // var brightness = (3*r+4*g+b)>>>3;
-            // data[i] = brightness;
-            // data[i+1] = brightness;
-            // data[i+2] = brightness;
-        }
-        idata.data = data;
-        // Draw the pixels onto the visible canvas
-        c.putImageData(idata,0,0);
-        // Start over!
-        setTimeout(function(){ draw(v,c,bc,scale); }, 0);
+    function draw(v, c, bc, scale) {
+      canvas.width = scale;
+      canvas.height = scale;
+      back.width = scale;
+      back.height = scale;
+      if (v.paused || v.ended) return false;
+      // First, draw it into the backing canvas
+      bc.drawImage(v, 0, 0, scale, scale);
+      // Grab the pixel data from the backing canvas
+      var idata = bc.getImageData(0, 0, scale, scale);
+      var data = idata.data;
+      // Loop through the pixels, turning them grayscale
+      for (var i = 0; i < data.length; i += 4) {
+        // var r = data[i];
+        // var g = data[i+1];
+        // var b = data[i+2];
+        // var brightness = (3*r+4*g+b)>>>3;
+        // data[i] = brightness;
+        // data[i+1] = brightness;
+        // data[i+2] = brightness;
+      }
+      idata.data = data;
+      // Draw the pixels onto the visible canvas
+      c.putImageData(idata, 0, 0);
+      // Start over!
+      setTimeout(function () {
+        draw(v, c, bc, scale);
+      }, 0);
     }
-
-
-
-
-function playVideo() {
-    v.play();
-}
-
-function pauseVideo() {
-    v.pause();
-}
     
-function next() {
-    if (curIndex <= 5) {
+    function playVideo() {
+      v.play();
+    }
+    
+    function pauseVideo() {
+      v.pause();
+    }
+    
+    function next() {
+      if (curIndex <= 5) {
         curIndex = curIndex + 1;
-    } 
-    else {
+      } else {
         curIndex = 0;
+      }
+    
+      console.log(curIndex);
+      var song = content.songs[curIndex];
+      var aFile = song.audio;
+      var vFile = song.video;
+      v.src = vFile;
+      a.src = aFile;
+      v.play();
     }
-
-    console.log(curIndex);
-    var song = content.songs[curIndex];
-    var aFile = song.audio;
-    var vFile = song.video;
-    v.src = vFile;
-    a.src = aFile;
-    v.play();
     
-    
-}
-
-function prev() {
-    if (curIndex > 0) {
+    function prev() {
+      if (curIndex > 0) {
         curIndex = curIndex - 1;
-    } 
-    else {
+      } else {
         curIndex = 6;
+      }
+    
+      var song = content.songs[curIndex];
+      var aFile = song.audio;
+      var vFile = song.video;
+      v.src = vFile;
+      a.src = aFile;
+      v.play();
+    }
+    
+    //toggle display of text modal
+    function toggleText() {
+      var x = document.getElementById("text-modal");
+      if (x.style.top === "0px") {
+        x.style.top = "-100vh";
+      } else {
+        x.style.top = "0px";
+      }
     }
 
-    var song = content.songs[curIndex];
-    var aFile = song.audio;
-    var vFile = song.video;
-    v.src = vFile;
-    a.src = aFile;
-    v.play();
 
-}
+    function play1() {
+        v.src = content.songs[0].video;
+        a.src = content.songs[0].audio;
+        v.play();
+    }
+    
 
+    function play2() {
+        v.src = content.songs[1].video;
+        a.src = content.songs[1].audio;
+        v.play();
+    }
+    
+
+    function play3() {
+        v.src = content.songs[2].video;
+        a.src = content.songs[2].audio;
+        v.play();
+    }
+    
+    function play4() {
+        v.src = content.songs[3].video;
+        a.src = content.songs[3].audio;
+        v.play();
+    }
+
+    function play5() {
+        v.src = content.songs[4].video;
+        a.src = content.songs[4].audio;
+        v.play();
+    }
+
+    function play6() {
+        v.src = content.songs[5].video;
+        a.src = content.songs[5].audio;
+        v.play();
+    }
+
+    function play7() {
+        v.src = content.songs[6].video;
+        a.src = content.songs[6].audio;
+        v.play();
+    }
+    
